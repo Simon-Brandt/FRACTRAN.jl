1e2f0ae3ba7f9b070fcdd9224bcb2801c6236cd9
Simon-Brandt <simon.brandt@uni-greifswald.de>
2026-08-26 15:55:19 +0200

    Adjusted code style from template

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,24 @@
+#!/usr/bin/env julia
+
+# Author: Simon Brandt
+# E-Mail: simon.brandt@uni-greifswald.de
+# Last Modification: 2026-08-26
+
+using Test: @testset
+
+using Aqua: Aqua
+using JET: JET
+
 using FRACTRAN
-using Test
-using Aqua
-using JET
 
 @testset "FRACTRAN.jl" begin
+    # Test the code quality.
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(FRACTRAN)
     end
+
+    # Lint the code.
     @testset "Code linting (JET.jl)" begin
-        JET.test_package(FRACTRAN; target_defined_modules = true)
+        JET.test_package(FRACTRAN, target_defined_modules=true)
     end
-    # Write your tests here.
 end
