e0235ec47448b17da98612ac26391fbb5ef9737f
Simon-Brandt <simon.brandt@uni-greifswald.de>
2026-08-28 11:48:54 +0200

    Formatted pre-defined prime numbers as in sieve of Eratosthenes for clarity

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,7 +20,7 @@
 
 # Author: Simon Brandt
 # E-Mail: simon.brandt@uni-greifswald.de
-# Last Modification: 2026-08-27
+# Last Modification: 2026-08-28
 
 using Test: @test, @testset, @test_throws
 
@@ -43,52 +43,26 @@ using FRACTRAN
     # Test FRACTRAN's prime number generation.
     @testset "Prime number generation" begin
         primes = [
-              2,
-              3,
-              5,
-              7,
-             11,
-             13,
-             17,
-             19,
-             23,
-             29,
-             31,
-             37,
-             41,
-             43,
-             47,
-             53,
-             59,
-             61,
-             67,
-             71,
-             73,
-             79,
-             83,
-             89,
-             97,
-            101,
-            103,
-            107,
-            109,
-            113,
-            127,
-            131,
-            137,
-            139,
-            149,
-            151,
-            157,
-            163,
-            167,
-            173,
-            179,
+                   2,   3,        5,        7,
+             11,       13,                 17,       19,
+                       23,                           29,
+             31,                           37,
+             41,       43,                 47,
+                       53,                           59,
+             61,                           67,
+             71,       73,                           79,
+                       83,                           89,
+                                           97,
+            101,      103,                107,      109,
+                      113,
+                                          127,
+            131,                          137,      139,
+                                                    149,
+            151,                          157,
+                      163,                167,
+                      173,                          179,
             181,
-            191,
-            193,
-            197,
-            199,
+            191,      193,                197,      199,
         ]
 
         @test generate_primes(3, 5) == [3, 5]
